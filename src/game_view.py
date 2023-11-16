b615caf3c610890a9162c29cd6b5e9ceeac2e233
Cunoucu <148616834+Cunoucu@users.noreply.github.com>
2023-11-16 15:27:43 +0100

    Update game_view.py

--- src/game_view.py
+++ src/game_view.py
@@ -6,9 +6,9 @@ class game_view:
         self.grid = grid((675, 675), (20, 20))
     
     def run(self, display: pygame.Surface):
-        self.grid.values[0][4] = True
+        self.grid.values[0][3] = True
         self.grid.values[2][2] = True
-        self.grid.values[2][3] = True
+        self.grid.values[2][4] = True
         self.grid.values[1][7] = True
         while True:
             for e in pygame.event.get():
